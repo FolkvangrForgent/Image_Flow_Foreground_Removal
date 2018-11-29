7281a3d2491e6ea5b55492cf3292182715873dfd
Zore12345 <makerbot11@gmail.com>
2018-11-29 10:23:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
 	
-def get_optical_flow(Old_Image, New_Image, window_size=5, debug_level=0, mask=None):
+def get_optical_flow(Old_Image, New_Image, window_size=9, debug_level=0, mask=None):
 	kernel_x = np.array([[-1., 1.], [-1., 1.]])
 	kernel_y = np.array([[-1., -1.], [1., 1.]])
 	
@@ -39,15 +39,16 @@ def get_optical_flow(Old_Image, New_Image, window_size=5, debug_level=0, mask=No
 				eign2 = eigns[1]
 				if np.min(abs(np.linalg.eigvals(np.matmul(A.T, A)))) >= 1e-2:
 					if True:
-						x = np.linalg.inv(A.T @ A) @ A.T @ b
+						x = np.linalg.pinv(A.T @ A) @ A.T @ b
 						u[i,j] = x[0]
 						v[i,j] = x[1]
 	return (u,v)
 
-def get_optical_flow_triangle(oldImage, newImage, window_size=5, depth=6, debug_level=0, mask=None):
+def get_optical_flow_triangle(oldImage, newImage, window_size=9, depth=6, debug_level=0, mask=None):
 	oldI = oldImage
 	newI = newImage
-	finalU,finalV = get_optical_flow(oldI,newI,window_size=window_size,debug_level=debug_level-1,mask=mask)
+	n_w = window_size
+	finalU,finalV = get_optical_flow(oldI,newI,window_size=n_w,debug_level=debug_level-1,mask=mask)
 	for i in range(depth):
 		oldI = cv2.pyrDown(oldI)
 		newI = cv2.pyrDown(newI)
@@ -55,11 +56,15 @@ def get_optical_flow_triangle(oldImage, newImage, window_size=5, depth=6, debug_
 		if mask is not None:
 			mask = np.floor(cv2.resize(np.array(mask,dtype=float), (int(oldI.shape[1]),int(oldI.shape[0])), interpolation = cv2.INTER_LINEAR))
 		
-		u, v = get_optical_flow(oldI,newI,window_size=window_size,debug_level=debug_level-1, mask=mask)
+		n_w = int(n_w*(2/3))
+		if n_w%2 == 0:
+			n_w += 1
 		
-		while oldImage.shape[0]/u.shape[0]>=2.0:
+		u, v = get_optical_flow(oldI,newI,window_size=n_w,debug_level=debug_level-1, mask=mask)
+		
+		while oldImage.shape[0]/u.shape[0]>1.0:
 			u = cv2.pyrUp(u)
-		while oldImage.shape[0]/v.shape[0]>=2.0:
+		while oldImage.shape[0]/v.shape[0]>1.0:
 			v = cv2.pyrUp(v)
 		
 		finalU += cv2.resize(u, (oldImage.shape[1],oldImage.shape[0]), interpolation = cv2.INTER_LINEAR)
@@ -83,13 +88,17 @@ def plot_image(image, title=""):
 	plt.xticks([]), plt.yticks([]), plt.title(title), plt.imshow(image, cmap='gray')
 	plt.show()
 	
-def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level=0, threshold=5e-3, exportFlowVideo=False):
+def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level=0, threshold=5e-3, exportFlowVideo=False, window_size=9):
 	if (video.isOpened()):
 		for i in range(startframe):
 			ret,frame = video.read()
+			if frame is None:
+				return
 			
 		#get old frame data
 		ret,frame = video.read()
+		if frame is None:
+			return
 		old_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		
 		#set desired shape of image based on which side is longer
@@ -103,8 +112,10 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 		
 		#matricies to hold image fill data
 		final_image = np.zeros(shape, dtype=int)
-		set_pixels = np.zeros(shape, dtype=int)			#used to track which pixels are set
-		test_pixels = np.full(shape, 1, dtype=int)		#used to compare with set_pixels
+		set_pixels = np.full(shape, 1.0, dtype=float)
+		
+		previos_mags = np.zeros((5,shape[0],shape[1]))
+		previos_frames = np.zeros((3,shape[0],shape[1]))
 		
 		frame_counter = 0
 		
@@ -113,16 +124,23 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 		while(video.isOpened()):
 			for i in range(skipframes):
 				ret,frame = video.read()
+				if frame is None:
+					break
 			
-			if frame_counter == maxframes or np.array_equal(set_pixels, test_pixels) or not video.isOpened():
+			if frame is not None:
+				ret, frame = video.read()
+			
+			if ( maxframes!=-1 and frame_counter == maxframes) or not video.isOpened() or frame is None:
 				plot_image(final_image)
+				plot_image(set_pixels)
 				return
-			
-			ret, frame = video.read()
+				
 			new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 			new_frame = cv2.resize(new_frame, (shape[1],shape[0]), interpolation = cv2.INTER_LINEAR)
 			
-			u,v = get_optical_flow_triangle(old_frame,new_frame,debug_level=debug_level-1,mask=None)
+			print("On flow "+str(frame_counter))
+			
+			u,v = get_optical_flow_triangle(old_frame,new_frame,debug_level=debug_level-1,mask=None,window_size=window_size)
 			mag = np.sqrt(np.add(np.square(u),np.square(v)))
 			
 			if exportFlowVideo:
@@ -135,24 +153,38 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 					#yFlow.write(v_cv)
 					#magFlow.write(mag_cv)
 					if max_val == -1.0:
-						max_val = mag.max()
-					mag_t = np.clip(mag,0,max_val)
+						max_val = mag.sum()/(mag.shape[0]*mag.shape[1])
 					
-					im = Image.fromarray(np.uint8((mag_t*255)/max_val)).convert('RGB')
+					im = Image.fromarray(np.uint8((mag*255)/mag.max())).convert('RGB')
+					sim = Image.fromarray(np.uint8((np.clip(mag,0,max_val*2)*255)/max_val*2)).convert('RGB')
 					im.save(os.path.join("data","video","mag"+str(frame_counter)+".jpeg"))
+					sim.save(os.path.join("data","video","smag"+str(frame_counter)+".jpeg"))
 			
 			if debug_level>0:
 				plot_flow(old_frame,new_frame,(u,v))
+				
+			previos_mags[frame_counter%5] = mag
+			previos_frames[frame_counter%3] = old_frame
 			
-			for i in range(shape[0]):
-				for j in range(shape[1]): 
-					if mag[i,j]<threshold:
-						final_image[i,j] = old_frame[i,j]
-						set_pixels[i,j] = 1
+			if frame_counter>=4:
+			
+				magn = np.zeros(shape)
+				magn += (1)*previos_mags[(frame_counter)%5]
+				magn += (2)*previos_mags[(frame_counter-1)%5]
+				magn += (3)*previos_mags[(frame_counter-2)%5]
+				magn += (2)*previos_mags[(frame_counter-3)%5]
+				magn += (1)*previos_mags[(frame_counter-4)%5]
+				magn /= 12
+				
+				for i in range(shape[0]):
+					for j in range(shape[1]): 
+						if magn[i,j]<set_pixels[i,j]:
+							final_image[i,j] = previos_frames[(frame_counter+1)%3][i,j]
+							set_pixels[i,j] = magn[i,j]
 			
 			old_frame = new_frame
 			frame_counter += 1
 			
-video = load_video("data","people_4.mp4")
-go_through_flow(video, skipframes=0, startframe=100, maxframes=100, debug_level=0, exportFlowVideo=True)
+video = load_video("data","people_3.mp4")
+go_through_flow(video, skipframes=0, startframe=502, maxframes=-1, window_size=9, debug_level=0, exportFlowVideo=True)
 
