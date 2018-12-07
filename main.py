e4361b0a271634c8fcf8441cd32afb8fb5b09742
Zore12345 <makerbot11@gmail.com>
2018-12-06 20:34:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
 	
-def get_optical_flow(Old_Image, New_Image, window_size=9, debug_level=0):
+def lk_flow_fast(Old_Image, New_Image, window_size=9, debug_level=0):
 	kernel_x = np.array([[-1., 1.], [-1., 1.]])
 	kernel_y = np.array([[-1., -1.], [1., 1.]])
 	
@@ -42,6 +42,10 @@ def get_optical_flow(Old_Image, New_Image, window_size=9, debug_level=0):
 	
 	det = (window_sums[...,0]*window_sums[..., 1]-window_sums[..., 2]**2)
 	
+	det = np.nan_to_num(det)
+	#plot_image(det)
+	#np.seterr(all='warn', invalid='print')
+	
 	flow_u = np.where(det != 0, \
 		(-window_sums[..., 1] * window_sums[..., 3] + \
 		window_sums[..., 2] * window_sums[..., 4]) / det, \
@@ -57,27 +61,100 @@ def get_optical_flow(Old_Image, New_Image, window_size=9, debug_level=0):
 	del flow_u,flow_v
 
 	return (u,v)
+	
+def lk_flow_improved(Old_Image, New_Image, window_size, debug_level=0):
+	kernel_x = np.array([[-1., 1.], [-1., 1.]])
+	kernel_y = np.array([[-1., -1.], [1., 1.]])
+	
+	w = int(window_size/2)
+	u=np.zeros(Old_Image.shape)
+	v=np.zeros(Old_Image.shape)
+	
+	Ix = np.array(cv2.filter2D(np.array(Old_Image, dtype='float'),-1,kernel_x), dtype='float')
+	Iy = np.array(cv2.filter2D(np.array(Old_Image, dtype='float'),-1,kernel_y), dtype='float')
+	It = np.array(New_Image, dtype='float') - np.array(Old_Image, dtype='float')
+	
+	if debug_level>0:
+		plot_image(Ix, title="partial x derivative of old image")
+		plot_image(Iy, title="partial y derivative of old image")
+		plot_image(It, title="partial time derivative of images")
+		
+	paramaters = np.zeros(Old_Image.shape+(5,))
+	paramaters[..., 0] = cv2.GaussianBlur(np.array(Ix ** 2, dtype='float'),(window_size,window_size),0)
+	paramaters[..., 1] = cv2.GaussianBlur(np.array(Iy ** 2, dtype='float'),(window_size,window_size),0)
+	paramaters[..., 2] = cv2.GaussianBlur(np.array(Ix * Iy, dtype='float'),(window_size,window_size),0)
+	paramaters[..., 3] = cv2.GaussianBlur(np.array(Ix * It, dtype='float'),(window_size,window_size),0)
+	paramaters[..., 4] = cv2.GaussianBlur(np.array(Iy * It, dtype='float'),(window_size,window_size),0)
+	del Ix, Iy, It
 
-def get_optical_flow_triangle(oldImage, newImage, window_size=9, depth=4, debug_level=0):
-	oldI = oldImage
-	newI = newImage
-	finalU,finalV = get_optical_flow(oldI,newI,window_size=window_size,debug_level=debug_level-1)
-	for i in range(depth):
-		oldI = cv2.pyrDown(oldI)
-		newI = cv2.pyrDown(newI)
+	
+	temp = np.nan_to_num(np.array(paramaters[..., 0]*paramaters[..., 1]-paramaters[..., 2]*paramaters[..., 2]))
+	ATA = np.nan_to_num(np.array([[paramaters[..., 0],paramaters[..., 2]],[paramaters[..., 2],paramaters[..., 1]]]))
+	eigns = np.nan_to_num(np.linalg.eigvals(ATA.T).T)
+	
+	if False:
+		test = np.where((eigns[0:,:] < eigns[1,:,:]), 1.0, 0.0)
+		test2 = np.where((eigns[0:,:] > eigns[1,:,:]), 1.0, 0.0)
+		plot_image(test[0])
+		plot_image(test2[0])
+	
+	
+	mineign = np.minimum(eigns[0,:,:],eigns[1,:,:]) 
+	maxeign = np.maximum(eigns[0,:,:],eigns[1,:,:]) 
+	
+	
+	u = np.where( ( (mineign > 1e-2) & ( (maxeign / mineign) < 255) ), \
+		paramaters[..., 4] * paramaters[..., 2] - paramaters[..., 3] * paramaters[..., 1], \
+		0.0)
+	v = np.where( ( (mineign > 1e-2) & ( (maxeign / mineign) < 255) ), \
+		-paramaters[..., 3] * paramaters[..., 2] - paramaters[..., 4] * paramaters[..., 0], \
+		0.0)
+	
+	u = np.nan_to_num(u/temp)
+	v = np.nan_to_num(v/temp)
+	
+	return (u,v)
+
+def generate_image_pyramid(image, minSize=(30,30)):
+	copyImage = image
+	pyramids = []
+	while copyImage.shape[0] > minSize[1] and copyImage.shape[1] > minSize[0]:
+		pyramids.insert(0, copyImage)
+		copyImage = cv2.pyrDown(copyImage)
+	return pyramids
+	
+def generate_window_size_pyramid(windowSize, depth, minSize=3):
+	sizes = []
+	for _ in range(depth):
+		if int(windowSize)%2 == 0:
+			windowSize = int(windowSize)+1
+		sizes.insert(0, windowSize)
+		windowSize = int(windowSize/2)
+		if windowSize<minSize:
+			windowSize=minSize
+	return sizes
+	
+def flow_warp(image, u, v):
+	u = np.array(cv2.pyrUp(u, dstsize=(image.shape[1],image.shape[0])), dtype=np.float32)
+	v = np.array(cv2.pyrUp(v, dstsize=(image.shape[1],image.shape[0])), dtype=np.float32)
+	return cv2.remap(image,u,v,cv2.INTER_LINEAR)
+	
+def coarse_to_fine_lk_flow(oldImage, newImage, window_size=9, debug_level=0):
+	oldImagePyramids = generate_image_pyramid(oldImage)
+	newImagePyramids = generate_image_pyramid(newImage)
+	windowSizePyramid = generate_window_size_pyramid(window_size, len(oldImagePyramids)) 
+	finalU = np.zeros(oldImage.shape)
+	finalV = np.zeros(oldImage.shape)
+	for i in range(len(oldImagePyramids)):
 		
-		window_size=int(window_size/2)
-		if (window_size%2 == 0):
-			window_size+=1
-		if (window_size<3):
-			window_size=3
+		u,v = lk_flow_improved(oldImagePyramids[i],newImagePyramids[i],windowSizePyramid[i],debug_level=debug_level-1)
+		plot_image(oldImagePyramids[i])
+		plot_image(newImagePyramids[i])
+		plot_image(u)
+		plot_image(v)
 		
-		u, v = get_optical_flow(oldI,newI,window_size=window_size,debug_level=debug_level-1)
-		
-		while oldImage.shape[0]/u.shape[0]>1.0:
-			u = cv2.pyrUp(u)
-		while oldImage.shape[0]/v.shape[0]>1.0:
-			v = cv2.pyrUp(v)
+		if i+1 != len(oldImagePyramids):
+			oldImagePyramids[i+1]=flow_warp(oldImagePyramids[i+1],u,v)
 			
 		finalU += cv2.resize(u, (oldImage.shape[1],oldImage.shape[0]), interpolation = cv2.INTER_LINEAR)
 		finalV += cv2.resize(v, (oldImage.shape[1],oldImage.shape[0]), interpolation = cv2.INTER_LINEAR)
@@ -97,7 +174,7 @@ def plot_flow(oldImage, newImage, flow):
 	plt.show()
 	
 def plot_image(image, title=""):
-	plt.xticks([]), plt.yticks([]), plt.title(title), plt.imshow(image, cmap='gray')
+	plt.xticks([]), plt.yticks([]), plt.title(title), plt.imshow(image)
 	plt.show()
 	
 def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level=0, threshold=5e-3, exportFlowVideo=False, window_size=9):
@@ -118,6 +195,7 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 			shape = (int(1280),int(720))
 		else:
 			shape = (int(720),int(1280))
+		#shape = old_frame.shape
 		
 		#scale old_frame down to desired shape
 		old_frame = cv2.resize(old_frame, (shape[1],shape[0]), interpolation = cv2.INTER_LINEAR)
@@ -142,8 +220,8 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 				ret, frame = video.read()
 			
 			if ( maxframes!=-1 and frame_counter == maxframes) or not video.isOpened() or frame is None:
-				plot_image(final_image, title="Produced Image")
-				plot_image(set_pixels, title="Cofidence")
+				plot_image(final_image, title="Produced Image", cmap='gray')
+				plot_image(set_pixels, title="Cofidence", cmap='gray')
 				return
 				
 			new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -156,7 +234,7 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
                        minDistance = 7,
                        blockSize = 7 )
 			
-			u,v = get_optical_flow_triangle(old_frame,new_frame,debug_level=debug_level-1,window_size=window_size)
+			u,v = coarse_to_fine_lk_flow(old_frame,new_frame,debug_level=debug_level-1,window_size=window_size)
 			mag = np.sqrt(np.add(np.square(u),np.square(v)))
 			
 			
@@ -187,38 +265,30 @@ def go_through_flow(video, skipframes=0, startframe=0, maxframes=10, debug_level
 					if max_set_val == -1.0:
 						max_set_val = set_pixels.max()
 						
-					flowoo = np.zeros(shape + (3,), dtype=np.uint8)
-					flowii = np.uint8(np.array([np.clip(u*125,0,255),np.abs(np.clip(u*125,-255,0)),mag*0]))
-					flowoo[:,:,0] = flowii[0,:,:]
-					flowoo[:,:,1] = flowii[1,:,:]
-					flowoo[:,:,2] = flowii[2,:,:]
-					magl = Image.fromarray(flowoo,'RGB')
-					magl.save(os.path.join("data","video","u"+str(frame_counter)+".jpeg"))
+					flowenc = np.zeros(shape + (3,), dtype=np.uint8)
+					flowenc[:,:,0] = np.arctan(v,u)*57.2958
+					flowenc[:,:,1] = np.clip(mag*125,0,255)
+					flowenc[:,:,2] = 200
+					encodedFlow = Image.fromarray(flowenc,'HSV').convert('RGB')
+					encodedFlow.save(os.path.join("data","video","flow"+str(frame_counter)+".jpeg"))
 					
-					flowii = np.uint8(np.array([np.clip(v*125,0,255),np.abs(np.clip(v*125,-255,0)),mag*0]))
-					flowoo[:,:,0] = flowii[0,:,:]
-					flowoo[:,:,1] = flowii[1,:,:]
-					flowoo[:,:,2] = flowii[2,:,:]
-					magl = Image.fromarray(flowoo,'RGB')
-					magl.save(os.path.join("data","video","v"+str(frame_counter)+".jpeg"))
-					
-					
-					flowii = np.uint8(np.array([np.clip(mag*125,0,255),np.clip(mag*125,0,255),np.clip(mag*125,0,255)]))
-					flowoo[:,:,0] = flowii[0,:,:]
-					flowoo[:,:,1] = flowii[1,:,:]
-					flowoo[:,:,2] = flowii[2,:,:]
-					magl = Image.fromarray(flowoo,'RGB')
-					magl.save(os.path.join("data","video","mag"+str(frame_counter)+".jpeg"))
-					
-					
-					con = Image.fromarray(np.uint8((set_pixels*255)/max_set_val)).convert('RGB')
-					fin = Image.fromarray(np.uint8(final_image)).convert('RGB')
-					con.save(os.path.join("data","video","con"+str(frame_counter)+".jpeg"))
-					fin.save(os.path.join("data","video","fin"+str(frame_counter)+".jpeg"))
+					#con = Image.fromarray(np.uint8((set_pixels*255)/max_set_val)).convert('RGB')
+					final = Image.fromarray(np.uint8(final_image)).convert('RGB')
+					#con.save(os.path.join("data","video","confidence"+str(frame_counter)+".jpeg"))
+					final.save(os.path.join("data","video","final"+str(frame_counter)+".jpeg"))
+			else:
+				if exportFlowVideo:
+					flowenc = np.zeros(shape + (3,), dtype=np.uint8)
+					flowenc[:,:,0] = np.arctan(v,u)*57.2958
+					flowenc[:,:,1] = np.clip(mag*125,0,255)
+					flowenc[:,:,2] = 200
+					encodedFlow = Image.fromarray(flowenc,'HSV').convert('RGB')
+					encodedFlow.save(os.path.join("data","video","flow"+str(frame_counter)+".jpeg"))
 			
 			old_frame = new_frame
 			frame_counter += 1
 			
 video = load_video("data","morepeople.mp4")
-go_through_flow(video, skipframes=0, startframe=10, maxframes=100, window_size=7, debug_level=0, exportFlowVideo=True)
+video2 = load_video("data","simple2.avi")
+go_through_flow(video, skipframes=0, startframe=0, maxframes=200, window_size=21, debug_level=0, exportFlowVideo=True)
 
